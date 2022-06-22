e2f4571e206696a1330ae6252635221069465810
its-me-bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-06-22 19:04:25 +0530

    sum of two numbers

---
+++ sum_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a , b;
+    cout << "Enter two Numbers :" << endl;
+
+    cin >> a;
+    cin >> b;
+    cout << (a + b) << endl;
+
+}
